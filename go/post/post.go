d7092cf28749c0100e2db7c7c493ba29784b13e3
jspark2000 <idpjs2000@gmail.com>
2024-07-18 11:17:10 +0000

    fix: formatting

--- go/post/post.go
+++ go/post/post.go
@@ -1,12 +1,12 @@
 package post
 
 type ConsumedPost struct {
-  Id int `json:"id"`
-  Title string `json:"title"`
-  Content string `json:"content"`
+	Id      int    `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
 }
 
 type ProducedPost struct {
-  Id int `json:"id"`
-  Status string `json:"status"`
+	Id     int    `json:"id"`
+	Status string `json:"status"`
 }
